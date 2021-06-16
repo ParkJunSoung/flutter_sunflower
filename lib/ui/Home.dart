78c9bab99bc28fd43eeb5164df0842f21fea62aa
준성 <madox9999@gmail.com>
2021-06-16 09:22:25 +0900

    MyGardenPage_뼈대

---
+++ lib/ui/Home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sunflower/ui/my_garden_page.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              Icon(Icons.menu,color: Colors.black,),
+            ],
+            centerTitle: true,
+            backgroundColor: Color(0xff48b978),
+            bottom: TabBar(
+              labelColor: Colors.black,
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car),text: "MY GARDEN"),
+                Tab(icon: Icon(Icons.directions_transit),text: "PLANT LIST"),
+              ],
+            ),
+            title: Text('Sunflower',style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.normal)),
+          ),
+          body: TabBarView(
+            children: [
+              MyGardenPage(),
+              Icon(Icons.directions_transit),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/my_garden_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyGardenPage extends StatelessWidget {
+  const MyGardenPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff48b978),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Your garden is empty',
+            style: TextStyle(fontSize: 22),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('ADD PLANT',style: TextStyle(color: Colors.black),),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Color(0xffc8c84e)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0),
+              ),
+            ))),
+          )
+        ],
+      ),
+    );
+  }
+}
