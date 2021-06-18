d1c15cd9e13c6e41c76c9f313b778acc62603311
준성 <madox9999@gmail.com>
2021-06-18 16:36:36 +0900

    Provider_PlatListPage....

--- lib/ui/plat_list_page.dart
+++ lib/ui/plat_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sunflower/d/provider.dart';
 import 'package:provider/provider.dart';
+
 class PlatListPage extends StatelessWidget {
   const PlatListPage({Key key}) : super(key: key);
 
@@ -8,27 +9,23 @@ class PlatListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var list = context.read<SunFlowerProvider>();
     return GridView.builder(
+      shrinkWrap: true,
       itemCount: list.plant.length,
-      itemBuilder: (context, index) => Container(
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(90.0),
-          bottomLeft: Radius.circular(90.0),
-        ),
-      ),
-        child: Column(
-          children: [
-            GridTile(
-            child: Image.network('${list.plants[index]}',fit: BoxFit.fill,),
-            ),
-            Text('${list.plant[index]}')
-          ],
+      itemBuilder: (context, index) => Card(
+        elevation: 5,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage("${list.plants[index]}",),
+                  fit: BoxFit.fill,
+                  alignment: Alignment.topCenter)),
+          child: Text('${list.plant[index]}'),
         ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        childAspectRatio: 2 / 2,
+        childAspectRatio: MediaQuery.of(context).size.width /
+            (MediaQuery.of(context).size.height / 2),
       ),
     );
   }
